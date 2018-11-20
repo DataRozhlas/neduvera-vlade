e1e454fda31498ded067e0fc27036f202d10a62b
Petr Kočí <petr.koci@samizdat.cz>
2018-11-20 23:35:36 +0100

    propojená a vyfiltrovaná data

---
+++ R/analyse.R
@@ -0,0 +1,40 @@
+library(jsonlite)
+
+getIdOsoby <- function(idp) {
+  result <- poslanec %>% filter(id_poslanec==idp) %>% select(id_osoba)
+  return(as.numeric(result[1,1]))
+}
+
+najdiPoslance <- function(ido) {
+  osoby %>% filter(id_osoba==ido)
+} 
+
+najdiKlub <- function(ido, idh) {
+  cas_hlasovani <- hlasovani %>% filter(id_hlasovani==idh) %>% select(datetime) 
+  platna <- zarazeni %>% filter(id_osoba==ido) %>% filter(cl_funkce==0) %>% filter(od_o < cas_hlasovani[[1]]) %>% filter(do_o > cas_hlasovani[[1]])
+  for (i in platna$id_of) {
+    organ <- organy %>% filter(id_organ==i)
+    if (organ$id_typ_organu==1) {return(organ[1,4][[1]])}
+  }
+  
+}
+
+createJSON <- function(idh) {
+  result <- data.frame()
+  posl_hlasy <- hlasovani_poslanci %>% filter(id_hlasovani==idh)
+  for (i in 1:nrow(posl_hlasy)) {
+    result <- rbind(result, data.frame(najdiPoslance(getIdOsoby(posl_hlasy[i,1][[1]])), klub=najdiKlub(getIdOsoby(posl_hlasy[i,1][[1]]), idh), vysledek=posl_hlasy[i,3] ))        
+  }
+  result <- result %>% select(j=jmeno, p=prijmeni, k=klub, v=vysledek)
+  result <- list(result)
+  return(toJSON(result))
+}
+
+
+createJSON(38766)
+
+najdiKlub(getIdOsoby(659), 38766)
+
+najdiPoslance(getIdOsoby(659))
+
+hlasovani_poslanci %>% filter(id_hlasovani==38766)
--- R/read.R
+++ R/read.R
@@ -1,18 +1,27 @@
-library(readr)
-files <- c("")
+library(tidyverse)
 
 # načíst poslance
 download.file("https://www.psp.cz/eknih/cdrom/opendata/poslanci.zip", "poslanci.zip")
 unzip("poslanci.zip")
 osoby <- read_delim("osoby.unl", "|", locale = locale(encoding="windows-1250"), col_names = c("id_osoba", "pred", "jmeno", "prijmeni", "za", "narozeni", "pohlavi", "zmena", "umrti"))
+poslanec <- read_delim("poslanec.unl", "|", locale = locale(encoding="windows-1250"), col_names = c("id_poslanec", "id_osoba", "id_kraj", "id_kandidatka", "id_obdobi", "web", "ulice", "obec", "psc", "email", "telefon", "fax", "psp_telefon", "facebook", "foto"))
 organy <- read_delim("organy.unl", "|", locale = locale(encoding="windows-1250"), col_names = c("id_organ", "organ_id_organ", "id_typ_organu", "zkratka", "nazev_organu_cz", "nazev_organu_en", "od_organ", "do_organ", "priorita", "cl_organ_base"))
+typ_organu <- read_delim("typ_organu.unl", "|", locale = locale(encoding="windows-1250"), col_names = c("id_typ_org", "typ_id_typ_org", "nazev_typ_org_cz", "nazev_typ_org_en", "typ_org_obecny", "priorita"))
 zarazeni <- read_delim("zarazeni.unl", "|", locale = locale(encoding="windows-1250"), col_names = c("id_osoba", "id_of", "cl_funkce", "od_o", "do_o", "od_f", "do_f"))
 file.remove(list.files(pattern="*.unl|*.zip"))
 
 # načíst všechna hlasování
 roky <- c(2002, 2006, 2010, 2013, 2017)
 
-hlasovani <- data.frame(id_poslanec=numeric(), id_hlasovani=numeric(), vyseledek=character())
+hlasovani <- data.frame() 
+hlasovani_poslanci <- data.frame(id_poslanec=numeric(), id_hlasovani=numeric(), vyseledek=character())
+
+spojHlasovani <- function(rok) {
+  download.file(paste0("https://www.psp.cz/eknih/cdrom/opendata/hl-", rok, "ps.zip"), paste0("hl-", rok, "ps.zip"))
+  unzip(paste0("hl-", rok, "ps.zip"))
+  return(read_delim(paste0("hl", rok, "s.unl"), "|", locale = locale(encoding="windows-1250"), col_names = c("id_hlasovani", "id_organ", "schuze", "cislo", "bod", "datum", "cas", "pro", "proti", "zdrzel", "nehlasoval", "prihlaseno", "kvorum", "druh_hlasovani", "vysledek", "nazev_dlouhy", "nazev_kratky")))
+  file.remove(list.files(pattern="*.unl|*.zip"))
+}
 
 spojHlasy <- function(rok) {
   download.file(paste0("https://www.psp.cz/eknih/cdrom/opendata/hl-", rok, "ps.zip"), paste0("hl-", rok, "ps.zip"))
@@ -27,7 +36,11 @@ spojHlasy <- function(rok) {
 }
 
 for (j in roky) {
-  hlasovani <- rbind(hlasovani, spojHlasy(j))
+  hlasovani_poslanci <- rbind(hlasovani_poslanci, spojHlasy(j))
 }
 
+for (j in roky) {
+  hlasovani <- rbind(hlasovani, spojHlasovani(j))
+}
 
+hlasovani$datetime <- as.POSIXct(paste(hlasovani$datum, hlasovani$cas), format = "%d.%m.%Y %H:%M:%S")
